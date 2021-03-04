8c6ee739106e0ffdd3054434d8f0eb8b5e888389
Janelle <jmbnw9@gmail.com>
2021-03-03 19:42:48 -0600

    updating file

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -29,7 +29,7 @@ var chartGroup = svg.append("g")
 // Step 3:
 // Import data from the data.csv file
 // =================================
-d3.csv("data.csv").then(function(data) {
+d3.csv("data/data.csv").then(function(data) {
 
   // Format the data
   data.forEach(function(data) {
@@ -37,43 +37,22 @@ d3.csv("data.csv").then(function(data) {
     data.healthcare = +data.healthcare;
   });
 
-  // Step 5: Create the scales for the chart
+  // Step 4: Create the scales for the chart
   // =================================
-  var xTimeScale = d3.scaleTime()
-    .domain(d3.extent(donutData, d => d.date))
+  var xLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.poverty)])
     .range([0, width]);
 
-  var yLinearScale = d3.scaleLinear().range([height, 0]);
+  var yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.healthcare)])
+    .range([height, 0]);
 
-  // Step 6: Set up the y-axis domain
-  // ==============================================
-  // @NEW! determine the max y value
-  // find the max of the morning data
-  var morningMax = d3.max(donutData, d => d.morning);
-
-  // find the max of the evening data
-  var eveningMax = d3.max(donutData, d => d.evening);
-
-  var yMax;
-  if (morningMax > eveningMax) {
-    yMax = morningMax;
-  }
-  else {
-    yMax = eveningMax;
-  }
-
-  // var yMax = morningMax > eveningMax ? morningMax : eveningMax;
-
-  // Use the yMax value to set the yLinearScale domain
-  yLinearScale.domain([0, yMax]);
-
-
-  // Step 7: Create the axes
+  // Step 5: Create the axes
   // =================================
-  var bottomAxis = d3.axisBottom(xTimeScale).tickFormat(d3.timeFormat("%d-%b"));
+  var bottomAxis = d3.axisBottom(xLinearScale);
   var leftAxis = d3.axisLeft(yLinearScale);
 
-  // Step 8: Append the axes to the chartGroup
+  // Step 6: Append the axes to the chartGroup
   // ==============================================
   // Add x-axis
   chartGroup.append("g")
@@ -83,7 +62,7 @@ d3.csv("data.csv").then(function(data) {
   // Add y-axis
   chartGroup.append("g").call(leftAxis);
 
-  // Step 9: Set up two line generators and append two SVG paths
+  // Step 7: Set up two line generators and append two SVG paths
   // ==============================================
 
   // Line generator for morning data
