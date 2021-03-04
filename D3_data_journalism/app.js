f396c6ec2ef9c1591bc81d516de8194860b9c234
Janelle <jmbnw9@gmail.com>
2021-03-04 17:05:40 -0600

    making changes

--- D3_data_journalism/app.js
+++ D3_data_journalism/app.js
@@ -18,7 +18,7 @@ var height = svgHeight - margin.top - margin.bottom;
 // and shift the latter by left and top margins.
 // =================================
 var svg = d3
-  .select("scatter")
+  .select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -28,7 +28,7 @@ var chartGroup = svg.append("g")
 
 // Import data from the data.csv file
 // =================================
-d3.csv("data/data.csv").then(function (data) {
+d3.csv("data.csv").then(function (data) {
 
   // Format the data
   data.forEach(function (data) {
@@ -39,7 +39,7 @@ d3.csv("data/data.csv").then(function (data) {
   // Create the scales for the chart
   // =================================
   var xLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.poverty)])
+    .domain([8, d3.max(data, d => d.poverty)])
     .range([0, width]);
 
   var yLinearScale = d3.scaleLinear()
@@ -66,26 +66,49 @@ d3.csv("data/data.csv").then(function (data) {
     .data(data)
     .enter()
     .append("circle")
-    .attr("cx", (d, i) => xScale(d.poverty))
-    .attr("cy", d => yScale(d.healthcare))
-    .attr("r", "5")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", "7")
     .attr("fill", "gray");
 
+  // Initialize tool tip
+  // ==============================
+  var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([80, -60])
+    .html(function (d) {
+      return (`${d.rockband}<br>Healthcare: ${d.healthcare}<br>Poverty: ${d.poverty}`);
+    });
 
+  // Step 7: Create tooltip in the chart
+  // ==============================
+  chartGroup.call(toolTip);
 
-  // Append a path for line1
-  chartGroup
-    .append("path")
-    .attr("d", line1(donutData))
-    .classed("line green", true);
+  // Step 8: Create event listeners to display and hide the tooltip
+  // ==============================
+  circlesGroup.on("click", function (data) {
+    toolTip.show(data, this);
+  })
+    // onmouseout event
+    .on("mouseout", function (data, index) {
+      toolTip.hide(data);
+    });
 
-  // Append a path for line2
-  chartGroup
-    .data([donutData])
-    .append("path")
-    .attr("d", line2)
-    .classed("line orange", true);
+  // Create axes labels
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 5)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .attr("font-weight", "bold")
+    .text("Lacks Healthcare (%)");
 
+  chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("class", "axisText")
+    .attr("font-weight", "bold")
+    .text("In Poverty (%)");
 }).catch(function (error) {
   console.log(error);
 });
